2023f6bca84ef89e91f23fffbbde66abd0aaa92e
Arfan <shaifarfan08@gmail.com>
2022-09-12 01:50:08 +0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'eslint-config-next',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/jsx-props-no-spreading': 0,
+  },
+};
